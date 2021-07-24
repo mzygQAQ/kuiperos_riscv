de3c72e924f97370f5deb83bbe95b1b867ba75c4
guoxiangCN <mzygdeaq@163.com>
2021-07-24 15:23:12 +0800

    nomsg

--- defs.h
+++ defs.h
@@ -20,4 +20,12 @@ void  mem_page_free(void *p);
 
 // macros
 
+#if defined(__GNUC__) && __GNUC__ >=4
+#define LIKELY(x)	(__builtin_expect((x), 1))
+#define UNLIKELY(x) (__builtin_expect((x), 0))
+#else
+#define LIKELY(x)	(x)
+#define UNLIKELY(x)	(x)
+#endif
+
 #endif
